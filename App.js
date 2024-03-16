a4af51ada2b7b70a87a48bda89a65ead9209b7d5
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-16 12:52:36 +0530

    added jsx

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
 
+let age=23;
+var email= 'anand@gmail.com';
+const name = 'anand';
+
+function fruit(){
+  return('apple')
+}
+
 function App() {
   return (
     <View>
       <Text style={{fontSize: 30}}>Hello Text Component</Text>
-      <Text style={{fontSize: 25}}>Im Anand...</Text>
-      <Text style={{fontSize: 20}}>From Nagpur</Text>
-      <Text style={{fontSize: 15}}>440017</Text>
+      <Text style={{fontSize: 25}}>{name}</Text>
+      <Text style={{fontSize: 20}}>{age}</Text>
+      <Text style={{fontSize: 15}}>{email}</Text>
       <Button title="Press Here"></Button>
       <Button title="Press Here Too"></Button>
+      <Text style={{fontSize: 15}}>{fruit()}</Text>
     </View>
   );
 }
