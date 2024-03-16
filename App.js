fab90b5f9e6f7b8e6a507b27aaf1e4ddc08e0bf5
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-16 12:23:20 +0530

    added View, Button And Text components

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello Text Component</Text>
+      <Text style={{fontSize: 25}}>Im Anand...</Text>
+      <Text style={{fontSize: 20}}>From Nagpur</Text>
+      <Text style={{fontSize: 15}}>440017</Text>
+      <Button title="Press Here"></Button>
+      <Button title="Press Here Too"></Button>
+    </View>
+  );
+}
+
+export default App;
