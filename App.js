4619d552a9d821682b702f7f676edb4f4ca4d275
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-16 15:02:49 +0530

    added button and onpress event

--- App.js
+++ App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 
 function App() {
+  const fruit = (value) => {
+    console.warn(value);
+  };
   return (
     <View>
       <Text style={{fontSize: 30}}>Hello Text Component</Text>
-      <UserData />
-      <UserData />
-      <UserData />
+      <Button
+        title="On Press"
+        onPress={() => fruit('Hello Anand')}
+        color={'green'}></Button>
+      <Button title="On Press2" onPress={fruit} color={'red'}></Button>
     </View>
   );
 }
 
-function UserData() {
-  return (
-    <View>
-      <Text style={{fontSize: 30}}>Name: Anand</Text>
-      <Text style={{fontSize: 30}}>Age: 23</Text>
-      <Text style={{fontSize: 30}}>Email: anand@gmail.com</Text>
-    </View>
-  );
-}
 export default App;
