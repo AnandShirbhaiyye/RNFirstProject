74e9da61f08c3bb318f39cbcc52d6c184a86c2f6
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-16 14:50:28 +0530

    added functional componensts

--- App.js
+++ App.js
@@ -5,21 +5,20 @@ function App() {
   return (
     <View>
       <Text style={{fontSize: 30}}>Hello Text Component</Text>
-      <UserData/>
-      <UserData/>
-      <UserData/>
+      <UserData />
+      <UserData />
+      <UserData />
     </View>
   );
 }
 
-
-function UserData(){
-  return(
+function UserData() {
+  return (
     <View>
-       <Text style={{fontSize: 30}}>Name: Anand</Text>
-       <Text style={{fontSize: 30}}>Age: 23</Text>
-       <Text style={{fontSize: 30}}>Email: anand@gmail.com</Text>
+      <Text style={{fontSize: 30}}>Name: Anand</Text>
+      <Text style={{fontSize: 30}}>Age: 23</Text>
+      <Text style={{fontSize: 30}}>Email: anand@gmail.com</Text>
     </View>
-  )
+  );
 }
 export default App;
