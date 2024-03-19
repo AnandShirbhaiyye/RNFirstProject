fba6aab5635009ec4add9b5faa7e826f1a427922
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-19 23:36:17 +0530

    feat: added styles in react-native, inline style and internal style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, StyleSheet} from 'react-native';
 
 function App() {
   const [name, setName] = useState('Anand');
@@ -12,11 +12,29 @@ function App() {
 
   return (
     <View>
-      <Text style={{fontSize: 30, color: 'red'}}>Hello {name}</Text>
-      <Text style={{fontSize: 30, color: 'red'}}>Hello {data}</Text>
+      <Text style={{fontSize: 15, color: 'pink'}}>Style in Recat native</Text>
+      <Text style={styles.textBox}>Hello {name}</Text>
+      <Text style={styles.textBox}>Hello {data}</Text>
+      <Text style={styles.textBox}>hey Im Anand...Full Stack developer</Text>
       <Button title="On Press" onPress={userName} color={'red'}></Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textBox: {
+    color: 'white',
+    fontSize: 20,
+    backgroundColor: 'gray',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    height: 100,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+});
 
 export default App;
